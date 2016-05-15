407fe64d88c739511d5a3cee38372cd03fe3c73f
atunet <atunet@qq.com>
2016-05-15 15:39:41 +0800

    auto gen tolua source

--- Assets/Editor/Custom/CustomSettings.cs
+++ Assets/Editor/Custom/CustomSettings.cs
@@ -27,6 +27,7 @@ public static class CustomSettings
         typeof(UnityEngine.RenderSettings),
         typeof(UnityEngine.QualitySettings),
         typeof(UnityEngine.GL),
+            typeof(CSInterface),
     };
 
     //附加导出委托类型(在导出委托时, customTypeList 中牵扯的委托类型都会导出， 无需写在这里)
@@ -112,6 +113,8 @@ public static class CustomSettings
         _GT(typeof(BlendWeights)),           
         _GT(typeof(RenderTexture)),
         _GT(typeof(Rigidbody)),
+
+            _GT(typeof(CSInterface)),
     };
 
     public static List<Type> dynamicList = new List<Type>()
