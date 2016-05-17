7df03cac0fbc1addf18fb0d548ea67b246ff3b46
tufeixiang <atunet@qq.com>
2016-05-17 15:51:17 +0800

    rename proto dir name

--- Assets/Scripts/LuaScripts/Role/Role.lua
+++ Assets/Scripts/LuaScripts/Role/Role.lua
@@ -11,19 +11,16 @@ function ParseRoleList()
 	local revCmd = RolePb.RoleList()
 	revCmd:ParseFromString(CSInterface.s_recvBytes)
 
-	if 0 == table.getn(revCmd.rolebase) then
-		--[[
+	if 0 == table.getn(revCmd.rolebase) then		
 		local createCmd = RolePb.CreateRoleReq()
 		createCmd.roletype = 11103000
-		createCmd.rolename = "atunet1"
+		createCmd.rolename = "atunet2"
 		createCmd.avatar:append(1)
 		createCmd.avatar:append(1)
 		createCmd.avatar:append(1)
 		createCmd.avatar:append(1)
 		SendCmd(BaseTypePb.CREATE_ROLE_REQ, createCmd:SerializeToString())
 		print("role size is 0, req create role: " .. createCmd.rolename)
-		--]]
-		print("role list size is 0")
 	else
 		local onlineCmd = RolePb.SelectRoleOnline()
 		onlineCmd.roleid = revCmd.rolebase[1].roleid
