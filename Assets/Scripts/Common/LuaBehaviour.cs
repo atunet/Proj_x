155f535363a85b7d6bb2941113b51dd4e471238b
aTunet <aTunet>
2016-06-19 19:27:32 +0800

    add luabehaviour

---
+++ Assets/Scripts/Common/LuaBehaviour.cs
@@ -0,0 +1,292 @@
+﻿//对于战斗 update 交给脚本管理更方便
+
+using UnityEngine;
+using System.Collections;
+using System;
+using LuaInterface;
+
+public sealed class LuaBehaviour : MonoBehaviour
+{
+    private string m_luaFileName = null;
+
+    private LuaTable m_self = null;
+    private LuaFunction m_updateFunc = null;
+
+  
+    protected LuaState      luaState_;
+    protected LuaFunction   lupdate_;
+
+    public LuaTable script
+    {
+        get{ return self_; }
+    }
+
+    protected void Awake()
+    {
+        luaState_ = LuaMainInstance.Instance.luaState;
+        if(ScriptName == null)
+        {
+            Debug.LogError("The ScriptName must be set.");
+            return;
+        }
+
+        //require lua文件，得到返回的类
+        LuaTable metatable = (LuaTable)LuaMainInstance.Instance.require(ScriptName);
+        if(metatable == null)
+        {
+            Debug.LogError("Invalid script file '" + ScriptName + "', metatable needed as a result.");
+            return;
+        }
+
+        //从类中找到New函数
+        LuaFunction lnew = (LuaFunction)metatable["New"];
+        if(lnew == null)
+        {
+            Debug.LogError("Invalid metatable of script '" + ScriptName + "', function 'New' needed.");
+            return;
+        }
+
+        //执行New函数生成脚本对象
+        object[] results = lnew.Call(metatable, this);
+        if(results == null || results.Length == 0)
+        {
+            Debug.LogError("Invalid 'New' method of script '" + ScriptName + "', a return value needed.");
+            return;
+        }
+
+        //存贮脚本对象
+        self_ = (LuaTable)results[0];
+
+        //给脚本对象设置上常用的属性
+        self_["transform"] = transform;
+        self_["gameObject"] = gameObject;
+        self_["behaviour"] = this;
+
+        lupdate_ = (LuaFunction)self_["Update"];
+
+        //尝试调用脚本对象的Awake函数
+        CallMethod("Awake");
+    }
+
+    // Use this for initialization
+    protected void Start ()
+    {
+        //尝试调用脚本对象的Start函数
+        CallMethod("Start");
+    }
+
+    // Update is called once per frame
+    protected void Update ()
+    {
+        if(lupdate_ != null)
+        {
+            lupdate_.Call(self_);
+        }
+    }
+
+    protected void OnDestroy()
+    {
+        CallMethod("OnDestroy");
+
+        if(lupdate_ != null)
+        {
+            lupdate_.Dispose();
+            lupdate_ = null;
+        }
+
+        //销毁脚本对象
+        if(self_ != null)
+        {
+            self_.Dispose();
+            self_ = null;
+        }
+    }
+
+    protected object[] CallMethod(string func, params object[] args)
+    {
+        if (self_ == null)
+        {
+            return null;
+        }
+
+        LuaFunction lfunc = (LuaFunction)self_[func];
+        if(lfunc == null)
+        {
+            return null;
+        }
+
+        //等价于lua语句: self:func(...)
+        int oldTop = lfunc.BeginPCall();
+        lfunc.Push(self_);
+        lfunc.PushArgs(args);
+        lfunc.PCall();
+        object[] objs = luaState_.CheckObjects(oldTop);
+        lfunc.EndPCall();
+        return objs;
+    }
+}
+
+/*
+public class LuaBehaviour : MonoBehaviour
+{
+    [NoToLuaAttribute]
+    public bool createInCS = false;
+    [NoToLuaAttribute]
+    public string luaFile = null;
+    [NoToLuaAttribute]
+    public string tableName = null;
+    [NoToLuaAttribute]
+    public bool unique = true;
+
+    protected LuaFunction LuaNew = null;
+    protected LuaFunction LuaAwake = null;
+    protected LuaFunction LuaStart = null;
+    protected LuaFunction LuaOnDestroy = null;
+
+    protected LuaFunction LuaOnTriggerEnter = null;
+    protected LuaFunction LuaOnTriggerExit = null;
+
+    protected LuaTable self = null;
+    private LuaState luaState = null;    
+
+    public virtual void SetLuaTable(LuaTable table)
+    {
+        if (self == null)
+        {
+            self = table;
+            self.name = tableName != null ? tableName : gameObject.name;
+        }
+
+        if (self != null)
+        {
+            LuaAwake = self.GetLuaFunction("Awake") as LuaFunction;
+            LuaStart = self.GetLuaFunction("Start") as LuaFunction;
+            LuaOnDestroy = self.GetLuaFunction("OnDestroy") as LuaFunction;
+            LuaOnTriggerEnter = self.GetLuaFunction("OnTriggerEnter") as LuaFunction;
+            LuaOnTriggerExit = self.GetLuaFunction("OnTriggerExit") as LuaFunction;
+        }
+    }
+
+    protected void Awake()
+    {
+        luaState = LuaClient.GetMainState();
+
+        if (createInCS && luaState != null)
+        {            
+            luaState.Require(luaFile);
+            self = luaState.GetTable(tableName);
+
+            if (!unique)
+            {
+                LuaNew = self.GetLuaFunction("New");                                
+                LuaNew.BeginPCall();                
+                LuaNew.PCall();
+                SafeRelease(ref self);
+                self = LuaNew.CheckLuaTable();
+                self.name = tableName;
+                LuaNew.EndPCall();                                
+            }
+
+            self["gameObject"] = gameObject;
+            self["this"] = this;
+            SetLuaTable(self);
+
+            if (LuaAwake != null)
+            {
+                LuaAwake.BeginPCall();
+                LuaAwake.Push(self);
+                LuaAwake.PCall();
+                LuaAwake.EndPCall();
+            }            
+        }        
+    }
+
+    protected void SafeRelease(ref LuaFunction func)
+    {
+        if (func != null)
+        {
+            func.Dispose();
+            func = null;
+        }
+    }
+
+    protected void SafeRelease(ref LuaTable table)
+    {
+        if (table != null)
+        {
+            table.Dispose();
+            table = null;
+        }
+    }
+
+    protected void Release()
+    {
+        SafeRelease(ref LuaNew);
+        SafeRelease(ref LuaAwake);
+        SafeRelease(ref LuaStart);
+        SafeRelease(ref LuaOnDestroy);
+        SafeRelease(ref LuaOnTriggerEnter);
+        SafeRelease(ref LuaOnTriggerExit);
+
+        //SafeRelease(ref update);
+        //SafeRelease(ref lateUpdate);
+        //SafeRelease(ref fixedUpdate);
+        SafeRelease(ref self);   
+    }
+
+    protected void Start()
+    {
+        if (LuaStart != null)
+        {
+            LuaStart.BeginPCall();
+            LuaStart.Push(self);
+            LuaStart.PCall();
+            LuaStart.EndPCall();            
+        }
+
+        //beStart = true;
+        //AddUpdate();
+    }
+
+    protected void OnDestroy()
+    {
+        if (LuaOnDestroy != null && luaState != null)
+        {
+            LuaOnDestroy.BeginPCall();
+            LuaOnDestroy.Push(self);
+            LuaOnDestroy.PCall();
+            LuaOnDestroy.EndPCall();            
+            Release();
+        }
+    }
+
+    protected void OnApplicationQuit()
+    {
+        LuaOnDestroy = null;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (LuaOnTriggerEnter != null)
+        {
+            LuaOnTriggerEnter.BeginPCall();
+            LuaOnTriggerEnter.Push(self);
+            LuaOnTriggerEnter.Push(other);
+            LuaOnTriggerEnter.PCall();
+            LuaOnTriggerEnter.EndPCall();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (LuaOnTriggerExit != null)
+        {
+            LuaOnTriggerExit.BeginPCall();
+            LuaOnTriggerExit.Push(self);
+            LuaOnTriggerExit.Push(other);
+            LuaOnTriggerExit.PCall();
+            LuaOnTriggerExit.EndPCall();
+        }
+    }
+}
+*/
---
+++ Assets/Scripts/Common/UIBase.cs
@@ -0,0 +1,560 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using LuaInterface;
+/*
+[ExecuteInEditMode, RequireComponent(typeof(UIPanel))]
+public class UIBase : MonoBehaviour
+{
+    public bool beKeepDepth = false;
+    public bool beUseLua = true;
+    public bool beUnique = true;
+
+    public Action OnUIClosed = delegate { };
+
+    public static UIBase current = null;
+
+    protected bool beClose = false;
+
+    protected static LinkedList<UIBase> uiList = new LinkedList<UIBase>();
+    protected static LinkedList<UIBase> noDepthList = new LinkedList<UIBase>();
+    protected static int maxDepth2D = 1;
+    protected UIPanel[] panels = null;
+
+    private const int delta = 10;
+
+    protected Transform myTrans = null;
+    protected UIPanel panel = null;
+
+    protected static Dictionary<string, AssetRecorder> dict = new Dictionary<string, AssetRecorder>();
+
+    protected LuaState luaState = null;
+    protected string moduleName = string.Empty;
+
+    protected bool beSetLuaData = false;
+    protected LuaTable self = null;
+    private LuaFunction update = null;
+    private LuaFunction lateUpdate = null;
+    private LuaFunction fixedUpdate = null;
+    private bool beStart = false;
+
+    protected List<LuaBaseRef> funcList = new List<LuaBaseRef>();
+
+    public LuaTable table
+    {
+        get { return self; }
+    }
+
+    protected virtual void Init()
+    {
+        myTrans = transform;
+        panel = gameObject.GetComponent<UIPanel>();
+        UnGfx.Attach(UI2D.Root, myTrans);
+
+        if (!beKeepDepth)
+        {
+            panels = gameObject.GetComponentsInChildren<UIPanel>(true);
+            Array.Sort<UIPanel>(panels, (p1, p2) => { return p1.depth - p2.depth; });
+            AddToList();
+        }
+        else
+        {
+            noDepthList.AddLast(this);
+        }
+    }
+
+    void CallLuaFunction(string name)
+    {
+        LuaFunction func = luaState.GetFunction(name);
+
+        if (func != null)
+        {
+            func.BeginPCall();
+            func.Push(self);
+            func.PCall();
+            func.EndPCall();
+            func.Dispose();
+        }
+    }
+
+    protected void Awake()
+    {
+        luaState = LuaClient.GetMainState();
+        name = Utility.RemoveCloneString(name);
+        moduleName = name;
+        current = this;
+
+        if (beUseLua && Application.isPlaying)
+        {
+            if (InitLua())
+            {
+                CallLuaFunction(moduleName + ".Awake");
+            }
+        }
+    }
+
+    protected void Start()
+    {
+        if (beUseLua && Application.isPlaying)
+        {
+            InitLua();
+            CallLuaFunction(moduleName + ".Start");
+            AddUpdate();      
+            beStart = true;                
+        }
+    }
+
+    protected void OnEnable()
+    {
+        if (beStart)
+        {
+            AddUpdate();
+        }
+    }
+
+    protected void OnDisable()
+    {
+        RemoveUpdate();
+    }
+
+    void AddUpdate()
+    {
+        if (update != null)
+        {
+            LuaClient.Instance.UpdateEvent.Add(update, self);
+        }
+
+        if (lateUpdate != null)
+        {
+            LuaClient.Instance.LateUpdateEvent.Add(lateUpdate, self);
+        }
+
+        if (fixedUpdate != null)
+        {
+            LuaClient.Instance.FixedUpdateEvent.Add(fixedUpdate, self);
+        }
+    }
+
+    protected void SafeRelease(ref LuaFunction func)
+    {
+        if (func != null)
+        {
+            func.Dispose();
+            func = null;
+        }
+    }
+
+    void RemoveUpdate()
+    {
+        LuaClient inst = LuaClient.Instance;
+
+        if (inst)
+        {
+            inst.UpdateEvent.Remove(update, self);
+            inst.LateUpdateEvent.Remove(lateUpdate, self);
+            inst.FixedUpdateEvent.Remove(fixedUpdate, self);
+        }
+    }
+
+    protected void SafeRelease(ref LuaTable table)
+    {
+        if (table != null)
+        {
+            table.Dispose();
+            table = null;
+        }
+    }
+
+    bool InitLua()
+    {
+        if (!beSetLuaData && luaState != null)
+        {
+            beSetLuaData = true;
+            luaState.Require("UI/" + moduleName);
+
+            self = luaState.GetTable(moduleName);
+            update = self.RawGetLuaFunction("Update");            
+            lateUpdate = self.RawGetLuaFunction("LateUpdate");
+            fixedUpdate = self.RawGetLuaFunction("FixedUpdate");
+
+            if (!beUnique)
+            {
+                LuaFunction func = self.RawGetLuaFunction("New");
+                func.BeginPCall();                
+                func.PCall();
+                self = func.CheckLuaTable();
+                func.EndPCall();
+                func.Dispose();                
+            }
+
+            self.name = moduleName;
+            self["name"] = moduleName;
+            SetUserData();           
+            return true;
+        }
+
+        return false;
+    }
+
+    [NoToLuaAttribute]
+    public static UIProxy LoadUI(string uiName, Action<UIBase> OnLoad)
+    {
+        UIProxy proxy = new UIProxy();
+        int id = GetAssetID(uiName);
+
+        Action<IAssetFile> action = (file) =>
+        {
+            UIBase ui = OnLoadUI(file, uiName);
+            proxy.uiBase = ui;
+            OnLoad(ui);
+        };
+
+        AssetFileMgr.Instance.Open(id, action);
+        return proxy;
+    }
+
+    public static UIProxy LoadUI(string uiName, LuaFunction OnLoad)
+    {
+        UIProxy proxy = new UIProxy();
+        int id = GetAssetID(uiName);
+        OnLoad.name = uiName + ".OnLoadUI";
+
+        Action<IAssetFile> action = (file) =>
+        {
+            UIBase ui = OnLoadUI(file, uiName);
+            proxy.uiBase = ui;
+
+            if (ui.self != null)
+            {
+                OnLoad.BeginPCall();
+                OnLoad.Push(ui.self);
+                OnLoad.PCall();
+                OnLoad.EndPCall();                
+            }
+            else
+            {
+                OnLoad.BeginPCall();
+                OnLoad.Push(ui);
+                OnLoad.PCall();
+                OnLoad.EndPCall();                    
+            }
+
+            OnLoad.Dispose();
+        };
+
+        AssetFileMgr.Instance.Open(id, action);
+        return proxy;
+    }
+
+    public static UIProxy LoadUI(string uiName)
+    {
+        UIProxy proxy = new UIProxy();
+        int id = GetAssetID(uiName);
+
+        Action<IAssetFile> action = (file) =>
+        {
+            proxy.uiBase = OnLoadUI(file, uiName);
+        };
+
+        AssetFileMgr.Instance.Open(id, action);
+        return proxy;
+    }
+
+    protected static UIBase OnLoadUI(IAssetFile file, string uiName)
+    {
+        GameObject go = file.Read<GameObject>(uiName);
+        DestroyAsset de = UnGfx.GetSafeComponent<DestroyAsset>(go);
+        de.file = file;
+        UIBase ui = go.GetComponent<UIBase>();
+        ui.moduleName = uiName;
+        ui.Init();
+        return ui;
+    }
+
+    void SetUserData()
+    {
+        self["gameObject"] = gameObject;
+        self["transform"] = transform;
+        self["uiBase"] = this;
+    }
+
+    //兼容c#和lua，采用c#函数
+    public void AddListener(string objName, LuaFunction func)
+    {
+        Transform node = UnGfx.FindNode(transform, objName);
+        UIEventListener listener = UIEventListener.Get(node.gameObject);
+        func.name = string.Format("{0}.{1}.OnClick", moduleName , objName);
+
+        UIEventListener.VoidDelegate action = (go) =>
+        {
+            func.BeginPCall();
+            func.Push(go);
+            func.PCall();
+            func.EndPCall();
+        };
+
+        listener.onClick += action;
+        funcList.Add(func);
+    }
+
+    public void AddListener(string objName, LuaFunction func, LuaTable self)
+    {
+        Transform node = UnGfx.FindNode(transform, objName);
+        UIEventListener listener = UIEventListener.Get(node.gameObject);
+        func.name = string.Format("{0}.{1}.OnClick", moduleName, objName);
+
+        UIEventListener.VoidDelegate action = (go) =>
+        {
+            func.BeginPCall();
+            func.Push(self);
+            func.Push(go);
+            func.PCall();
+            func.EndPCall();
+        };
+
+        listener.onClick += action;
+        funcList.Add(func);
+        funcList.Add(self);
+    }
+
+
+    //兼容c#和lua，采用c#函数
+    public void AddListenerForInput(string objName, LuaFunction func)
+    {
+        Transform node = UnGfx.FindNode(transform, objName);
+        UIEventListener listener = UIEventListener.Get(node.gameObject);                
+        func.name = string.Format("{0}.{1}.OnInput", moduleName, objName);
+
+        UIEventListener.VoidDelegate action = (go) =>
+        {
+            func.BeginPCall();
+            func.Push(go);
+            func.PCall();
+            func.EndPCall();
+        };
+
+        listener.onSubmit += action;
+        funcList.Add(func);
+    }
+
+    public void AddListenerForInput(string objName, LuaFunction func, LuaTable self)
+    {
+        Transform node = UnGfx.FindNode(transform, objName);
+        UIEventListener listener = UIEventListener.Get(node.gameObject);
+        func.name = string.Format("{0}.{1}.OnInput", moduleName, objName);
+        
+
+        UIEventListener.VoidDelegate action = (go) =>
+        {
+            func.BeginPCall();
+            func.Push(self);
+            func.Push(go);
+            func.PCall();
+            func.EndPCall();            
+        };
+
+        listener.onSubmit += action;
+        funcList.Add(func);
+        funcList.Add(self);
+    }
+
+    public UICenterOnChild.OnCenterCallback VoidDelegateForCenter(LuaFunction func)
+    {
+        func.name = string.Format("{0}.OnCenter", moduleName);
+
+        UICenterOnChild.OnCenterCallback action = (go) =>
+        {
+            func.BeginPCall();
+            func.Push(go);
+            func.PCall();
+            func.EndPCall();            
+        };
+
+        funcList.Add(func);
+        return action;
+    }
+
+    public UICenterOnChild.OnCenterCallback VoidDelegateForCenter(LuaFunction func, LuaTable self)
+    {
+        func.name = string.Format("{0}.OnCenter", moduleName);
+
+        UICenterOnChild.OnCenterCallback action = (go) =>
+        {
+            func.BeginPCall();
+            func.Push(self);
+            func.Push(go);
+            func.PCall();
+            func.EndPCall();                        
+        };
+
+        funcList.Add(func);
+        return action;
+    }
+
+
+    [NoToLuaAttribute]
+    public static int GetAssetID(string uiName)
+    {
+        AssetRecorder asset = null;
+
+        if (!dict.TryGetValue(uiName, out asset))
+        {
+            string sql = string.Format("select * from assets where type = 5 and name = 'UI_{0}.unity3d'", uiName);
+            asset = TableMgr.Instance.QueryFirst<AssetRecorder>(sql);
+            dict.Add(uiName, asset);
+        }
+
+        return asset.id;
+    }
+
+    //[NoToLuaAttribute]
+    //public object[] CallLuaFunc(string funName, params object[] objs)
+    //{
+    //    string fName = string.Format("{0}.{1}", moduleName, funName);
+    //    return CallLuaFunction(fName, objs);
+    //}
+
+    public static void DestroyAll()
+    {
+        foreach (UIBase ui in uiList)
+        {
+            GameObject go = ui.gameObject;
+            ui.beClose = true;
+            ui.beUseLua = false;
+            ui.OnUIClosed();
+            GameObject.Destroy(go);
+        }
+
+        foreach (UIBase ui in noDepthList)
+        {
+            GameObject go = ui.gameObject;
+            ui.beClose = true;
+            ui.beUseLua = false;
+            ui.OnUIClosed();
+            GameObject.Destroy(go);
+        }
+
+        uiList.Clear();
+        noDepthList.Clear();
+    }
+
+    public void LogError(string str)
+    {
+        Type type = this.GetType();
+        Debugger.LogError(string.Format("{0} error: {1}", type.Name, str));
+    }
+
+    public virtual void Close()
+    {        
+        CloseInTime(10);
+    }
+
+    public virtual void CloseInTime(float time)
+    {
+        if (!beClose && Application.isPlaying)
+        {
+            if (beUseLua)
+            {
+                CallLuaFunction(moduleName + ".OnDestroy");                
+            }
+
+            beClose = true;
+            OnUIClosed();
+            RemoveFromList();
+            gameObject.SetActive(false);
+
+            if (time == 0)
+            {
+                GameObject.Destroy(gameObject);
+            }
+            else
+            {
+                GameObject.Destroy(gameObject, time);
+            }
+        }
+    }
+
+    protected virtual void AddToList()
+    {
+        AddToList(uiList, ref maxDepth2D);
+    }
+
+    protected virtual void RemoveFromList()
+    {
+        RemoveFromList(uiList, ref maxDepth2D, 1);
+    }
+
+    protected void AddToList(LinkedList<UIBase> list, ref int depth)
+    {
+        if (beKeepDepth)
+        {
+            return;
+        }
+
+        if (list.Count > 0)
+        {
+#if UNITY_EDITOR
+            if (list.Find(this) != null)
+            {
+                Debugger.LogError("UI {0} already in ui list", name);
+                return;
+            }
+#endif
+        }
+
+        depth = SetDepth(depth) + delta;
+        list.AddLast(this);
+    }
+
+    protected void RemoveFromList(LinkedList<UIBase> list, ref int depth, int beginDepth)
+    {
+        if (beKeepDepth || list.Count == 0)
+        {
+            noDepthList.Remove(this);
+            return;
+        }
+
+        list.Remove(this);
+        depth = beginDepth;
+
+        foreach (UIBase ui in list)
+        {
+            depth = ui.SetDepth(depth) + delta;
+        }
+    }
+
+    int SetDepth(int value)
+    {
+        int baseLine = panels[0].depth;
+        value -= baseLine;
+
+        for (int i = 0; i < panels.Length; i++)
+        {
+            panels[i].depth += value;
+        }
+
+        return panels[panels.Length - 1].depth;
+    }
+
+    protected void OnDestroy()
+    {
+        Close();
+
+        for (int i = 0; i < funcList.Count; i++)
+        {
+            if (funcList[i] != null)
+            {
+                funcList[i].Dispose();
+            }
+        }
+
+        funcList.Clear();
+
+        SafeRelease(ref update);
+        SafeRelease(ref lateUpdate);
+        SafeRelease(ref fixedUpdate);
+        SafeRelease(ref self);                
+    }
+}*/
